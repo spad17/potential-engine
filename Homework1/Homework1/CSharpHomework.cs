1a6b80219fac609a5111a3c62a863f4fe43034f7
Алексей Миргородский <mirgorodsky.alexey@gmail.com>
2025-02-23 20:19:49 +0300

    Task 1

--- Homework1/Homework1/CSharpHomework.cs
+++ Homework1/Homework1/CSharpHomework.cs
@@ -2,10 +2,17 @@
 {
     internal class CSharpHomework
     {
+        // Задание 1 функция которая обнуляет разряд десятков.
+        static int TensToZeroDigit(int number)
+        {
+            return (number / 100) * 100 + (number % 10);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World2!");
+            Console.WriteLine("задание 1");
+            Console.WriteLine($"Обнуление 10 у числа {123} = {TensToZeroDigit(123)}");
+            Console.WriteLine($"Обнуление 10 у числа {1234} = {TensToZeroDigit(1234)}");
+            Console.WriteLine($"Обнуление 10 у числа {12345} = {TensToZeroDigit(12345)}");
         }
     }
 }
